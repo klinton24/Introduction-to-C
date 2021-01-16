3f6c45f2192f07557e425806401297bb987b9d42
K Smith <klinton.jp@gmail.com>
2021-01-15 17:09:42 -0700

    Updated with next day's work

--- Hello World/main.c
+++ Hello World/main.c
@@ -11,5 +11,28 @@ int main()
     printf("There once was a man named %s.\n", characterName);
     printf("He was %d years old.\n", characterAge);
     printf("He really liked the name %s but did not like being %d.\n", characterName, characterAge);
+
+    int age = 40;
+    double gpa = 3.6;
+    char grade = 'A';
+    /*
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("You are %d years old.\nt", age);
+
+    printf("Enter your grade: ");
+    scanf("%c", &grade);
+    printf("Your grade is %c", grade);
+
+    char name[20];
+    printf("Enter your name: ");
+    scanf("%s", &name);
+    printf("Your grade is %s", name);
+    */
+    char fullName[20];
+    printf("Enter your full name: ");
+    fgets(fullName, 20, stdin);
+    printf("Your full name is %s", fullName);
+
     return 0;
 }
