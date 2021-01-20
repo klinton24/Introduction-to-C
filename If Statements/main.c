2466f7f79ee51f46055a81e17a0cce014bd48d2e
K Smith <klinton.jp@gmail.com>
2021-01-19 21:58:17 -0700

    Added Return and If statements

---
+++ If Statements/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max(int num1, int num2, int num3) {
+    int result;
+    if(num1>num2) {
+        if(num1>num3){
+            result=num1;
+        }
+    } else if(num2>num1){
+        if(num2>num3){
+            result=num2;
+        }
+    } else {
+        result =num3;
+    }
+    return result;
+}
+
+int main()
+{
+    printf("%d", max(4,30,100));
+    return 0;
+}
