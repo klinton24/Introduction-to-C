49e9e3414f79bf8988bc37e54e3b0f69062652f6
K Smith <klinton.jp@gmail.com>
2021-01-23 08:18:44 -0700

    added While Loop

---
+++ While Loop/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int index = 5;
+    while(index >=1) {
+        printf("%d\n", index);
+        index--;
+    }
+    printf("Blast off!");
+    return 0;
+}
